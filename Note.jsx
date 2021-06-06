dd16229a5958ccf712ba0d58ff723ca9fe4469ef
dheeraj1999-wq <83547771+dheeraj1999-wq@users.noreply.github.com>
2021-06-06 21:18:54 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1> JavaScript and React.js </h1>
+      <p>
+        This bootcamp is taken by shaurya sinha.This bootcamp taught Reactjs and
+        javascript grom the begining.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
